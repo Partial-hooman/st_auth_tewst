e550afd3b999bd9eb0132b6e724b63a319e4e2a7
Partial-hooman <105847724+Partial-hooman@users.noreply.github.com>
2023-04-19 14:56:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,22 @@ from google_auth_oauthlib.flow import Flow
 
 
 
-flow = Flow.from_client_secrets_file(
-    "client_secret_1023844332180-knicf5jgjp1d286076t71tp9ntjlbcr5.apps.googleusercontent.com.json",
-    scopes=['https://www.googleapis.com/auth/webmasters.readonly'],
-    redirect_uri='https://partial-hooman-fiverr-google-console-api-main-qxl9lf.streamlit.app/authentication')
+try:
+    st.write(st.experimental_get_query_params()['code'])
+    
+except:
+    
+
+    flow = Flow.from_client_secrets_file(
+                 "client_secret_1023844332180-knicf5jgjp1d286076t71tp9ntjlbcr5.apps.googleusercontent.com.json",
+                 scopes=['https://www.googleapis.com/auth/webmasters.readonly'],
+                 redirect_uri='https://partial-hooman-st-auth-tewst-main-rfcit3.streamlit.app/')
 
 
-auth_uri = flow.authorization_url()
+    auth_uri = flow.authorization_url()
 
-st.header("please authorize using the follow:")
+    st.header("please authorize using the follow:")
 
-link = f'[AUTHORIZE]({auth_uri[0]})'
-st.markdown(link, unsafe_allow_html=True)
+    link = f'[AUTHORIZE]({auth_uri[0]})'
+    st.markdown(link, unsafe_allow_html=True)
 
-
-
-st.write(st.experimental_get_query_params()['code'])
